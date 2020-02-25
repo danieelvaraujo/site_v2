2a735b61d6c0f712509a7db02da2826f04628c72
Daniel Valente Araújo <56133316+danieelvaraujo@users.noreply.github.com>
2020-02-25 18:23:12 -0300

    Server adicionado

--- src/Paginas/Contato/Contato.js
+++ src/Paginas/Contato/Contato.js
@@ -33,7 +33,7 @@ class Contato extends Component {
       disabled: true
     });
 
-    Axios.post("danielvalente.herokuapp.com/api/email", this.state)
+    Axios.post("/api/email", this.state)
       .then(res => {
         if (res.data.success) {
           this.setState({
